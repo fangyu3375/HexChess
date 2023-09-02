c5eab40735160e2a40d7b8b45cbb6e1f91901a84
Fang Yu <1846844726@qq.com>
2023-09-02 16:05:51 +0800

    MinmaxSearch1.0: share chessBoard, use destructor, force expand, bad performance

--- Minmax.cpp
+++ Minmax.cpp
@@ -8,13 +8,12 @@
 
 using namespace std;
 
-// typedef pair<int, pair<int, int>> PIP;
-
-const int SIZE = 11, MAX_DEPTH = 10, MAX_BRANCH= 15, INF = 0x3f3f3f3f;
+const int SIZE = 11, MAX_DEPTH = 5, MAX_BRANCH= 15, INF = 0x3f3f3f3f;
 
 int board[SIZE][SIZE] = { 0 };//本方1，对方-1，空白0
 enum Player{Red, Blue};
 bool isFirstPlayer;     // true为红方（连接上下边界），false为蓝方（连接左右边界）
+int redValue;
 
 struct Position {
     int x, y;
@@ -22,56 +21,36 @@ struct Position {
 
 struct MinmaxTree {
     int depth;
-    int selfboard[SIZE][SIZE];
+    double score;
     Position decision;
     MinmaxTree* parent;
     vector<MinmaxTree*> children;
-    // double alpha, beta;
-    double score;
-    MinmaxTree(int b[][SIZE], double sc, Position pos, MinmaxTree* t) 
-    :score(sc), decision(pos), parent(t)
-    {memcpy(selfboard, b, sizeof(int) * SIZE * SIZE);}
+    MinmaxTree(MinmaxTree* t = nullptr, int d = 0, Position pos = {-1, -1}, double sc = 1.) 
+    :parent(t), depth(d), decision(pos), score(sc) {}
+    ~MinmaxTree() {for (auto child : children) delete(child);}
     bool operator<(const MinmaxTree& t) {return score < t.score;}
+    void move(){board[decision.x][decision.y] = (depth & 1)? 1: -1;}  // 奇数层为我方走子，偶数层为对方走子
+    void revoke(){board[decision.x][decision.y] = 0;}
 };
 
-int bound[2] = {0, SIZE - 1};
-int distFrom[4][SIZE][SIZE];        // {0, 1, 2, 3} 分别表示 {上, 左, 下, 右}
 int dx[6] = {-1, -1, 0, 1, 1, 0}, dy[6] = {0, 1, 1, 0, -1, -1};  // 方向向量
-bool st[SIZE][SIZE];
 
-double evalute(MinmaxTree* node) {
-    return node->score;
-}
-
-void GenerateSearchSpace(MinmaxTree* node) {
-    if (node->depth >= MAX_DEPTH) return;
-
-    expand(node);              // 生成孩子节点，child->depth = node->depth + 1;
-    for (auto child : node->children) expand(child);
-}
-
-// 估值函数，用来对当前局面的先手优势的估值
-// Valuation function1.0: 使用电阻电路策略估值
-// 电阻电路策略：当前局面下，先手获胜需要的最小步数 / 后手获胜需要的最小步数
-// 可以将先手方落子处的电阻设为0，后手方落子处的电阻设为正无穷，空子处设为1，计算最短路问题
-// 电阻正无穷处无法落子，那么相当于边权为0和1最短路问题，可采用双端队列进行求解
-double expand(MinmaxTree* node) {
-    memset(distFrom, 0x3f, sizeof distFrom);
-    memset(st, false, sizeof st); 
-    int redValue = isFirstPlayer ? 1 : -1;  // 判断红方是board[][]中值为1的点还是-1的点
-    for (int d = 0; d < 4; ++d) {       // 对四个方向进行计算
-        int i, j;     // i表示枚举下标，j表示初始边界
+// 评估当前局面先手的优势，棋局保存在全局变量board[][]中
+double evalute() {
+    int dist[SIZE][SIZE], minDist[2];
+    for (int d = 0; d < 2; ++d) {       // 对上下和左右两个方向进行计算, d == 0:上下, d == 1:左右
+        memset(dist, -1, sizeof dist);
         int selfValue = (d & 1)? -redValue : redValue;  // selfValue表示连接d方向的棋子在board[][]中的值
-        j = (d & 2)? bound[1] : bound[0];   // 若d为2或3，初始边界为SIZE - 1，否则初始边界为0
 
         deque<pair<int, int>> q;
-        for (i = 0; i < SIZE; ++i) {            // 初始时，边界上的棋子入队
-            if (d & 1) swap(i, j);              // 若d为1或3，表示横向枚举，此时i为初始边界，j为枚举下标
+        for (int i = 0, j = 0; j < SIZE; ++j) {  // i表示枚举下标，j表示初始边界 
+            if (d & 1) swap(i, j);              // 若d为1，表示横向枚举，此时i为初始边界，j为枚举下标
 
-            if (node->selfboard[i][j] == selfValue) 
-                q.push_front({i, j}), distFrom[d][i][j] = 0, st[i][j] = true;
-            else if (node->selfboard[i][j] == 0) 
-                q.push_back({i, j}), distFrom[d][i][j] = 1, st[i][j] = true;
+            // 初始时，边界上的点入队
+            if (board[i][j] == selfValue) 
+                q.push_front({i, j}), dist[i][j] = 0;
+            else if (board[i][j] == 0) 
+                q.push_back({i, j}), dist[i][j] = 1;
 
             if (d & 1) swap(i, j);              
         }
@@ -81,57 +60,58 @@ double expand(MinmaxTree* node) {
             q.pop_front();
 
             int pos[2] = {t.first, t.second};
-            if (pos[d & 1] == SIZE - 1 - j) continue;// 到达对边界：若d为1或3，应判断t.second是否到达对边界
+            int distance = dist[pos[0]][pos[1]];
+            if (pos[d & 1] == SIZE - 1) {   // 到达对边界：若d为1，应判断t.second是否到达对边界
+                minDist[d & 1] = distance;
+                break;
+            }
 
             for (int i = 0; i < 6; ++i) {
                 int a = pos[0] + dx[i], b = pos[1] + dy[i];
-                if (a < 0 || a >= SIZE || b < 0 || b >= SIZE || st[a][b]) continue;
-                int value = node->selfboard[a][b], distance = distFrom[d][pos[0]][pos[1]];
-                if (value == -selfValue) continue;
-                if (value == 0)  q.push_back({a, b}), distFrom[d][a][b] = distance + 1; 
-                else q.push_front({a, b}), distFrom[d][a][b] = distance;
-                st[a][b] = true;
+                if (a < 0 || a >= SIZE || b < 0 || b >= SIZE || ~dist[a][b]) continue;
+                if (board[a][b] == -selfValue) continue;
+
+                if (board[a][b] == 0)  q.push_back({a, b}), dist[a][b] = distance + 1; 
+                else q.push_front({a, b}), dist[a][b] = distance;
             }
         }
     }
 
-    int cnt = 0;
+    // cout << minDist[0] << ' ' << minDist[1] << endl;
+    if (minDist[0] == 0) return INF;
+    return (double)minDist[1] / minDist[0];
+}
+
+// 朴素版扩展
+// 只限定扩展层数，而不要求子节点是否对己方有利
+void expand(MinmaxTree* node) {
+    if (node->depth >= MAX_DEPTH) return;
     for (int i = 0; i < SIZE; ++i)
         for (int j = 0; j < SIZE; ++j)
-            if (!node->selfboard[i][j]) {
-                int redScore = distFrom[0][i][j] + distFrom[2][i][j] - 1;
-                int blueScore = distFrom[1][i][j] + distFrom[3][i][j] - 1;
-                double score = (blueScore == 0)? INF : (double)redScore / blueScore;
-                // double ε = 1e-6;
-                if (score > 1.) {   //?
-                    cnt++;
-                    MinmaxTree *child = new MinmaxTree(node->selfboard, score, {i, j}, node);
-                    child ->selfboard[i][j] = redValue; //?
-                    node->children.push_back(child);
-                }
+            if (board[i][j] == 0) {
+                MinmaxTree* child = new MinmaxTree(node, node->depth + 1, {i, j});
+                node->children.push_back(child);
+                child->move(), expand(child), child->revoke();
             }
-    if (cnt > MAX_BRANCH) {
-        sort(node->children.begin(), node->children.end());
-        while (cnt > MAX_BRANCH)  {
-            delete(node->children.back());
-            node->children.pop_back();
-        }
-    }
-    return 0.0;
+                
 }
 
 double AlphaBeta(MinmaxTree* node, int depth, double alpha, double beta, Player player) {
-    if (depth == 0) return evalute(node);
+    if (depth == 0) return evalute();
 
     if (player == Red) {
         for (auto &child : node->children) {
+            child->move();                     // 走子
             alpha = max(alpha, AlphaBeta(child, depth - 1, alpha, beta, Blue));
             if (alpha >= beta) break;          // alpha剪枝 
+            child->revoke();                   // 回溯
         }
         return alpha;
     } else {
         for (auto &child : node->children) {
+            child->move();
             beta = min(beta, AlphaBeta(child, depth - 1, alpha, beta, Red));
+            child->revoke();   
             if (beta <= alpha) break;
         }
         return beta;
@@ -168,8 +148,9 @@ int main()
 	//此时board[][]里存储的就是当前棋盘的所有棋子信息,x和y存的是对方最近一步下的棋
 
     if(board[1][2] == 1) isFirstPlayer= true; // 我方为红方
+    redValue = isFirstPlayer ? 1 : -1;  // 判断红方是board[][]中值为1的点还是-1的点
 
-    MinmaxTree* gameRoot = new MinmaxTree(board, -1, {-1, -1}, nullptr);
+    MinmaxTree* gameRoot = new MinmaxTree();
     expand(gameRoot);
     if (isFirstPlayer) AlphaBeta(gameRoot, MAX_DEPTH, -INF, INF, Red);
     else AlphaBeta(gameRoot, MAX_DEPTH, -INF, INF, Blue);
